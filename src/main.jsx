4217f1752490833f5d18e28158a8464c80be3faa
Timothy Rogers <Timothy.Rogers@iag.com.au>
2016-09-24 13:32:02 +1000

    End of Section3 - Lecture 26

--- src/main.jsx
+++ src/main.jsx
@@ -3,3 +3,7 @@ var ReactDOM = require('react-dom');
 var ListManager = require('./components/ListManager.jsx');
 
 ReactDOM.render(<ListManager title="Ingredients" />, document.getElementById('ingredients'));
+
+ReactDOM.render(<ListManager title="To Do" />, document.getElementById('todo'));
+
+ReactDOM.render(<ListManager title="Christmas" headingColor="#b31217" />, document.getElementById('xmas'));
